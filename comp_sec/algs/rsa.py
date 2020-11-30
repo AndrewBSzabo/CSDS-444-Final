130dbdccf05461d9ca9a153c61a55a5985efea51
AndrewBSzabo <szabomon@outlook.com>
2020-11-30 15:08:33 -0500

    reformat rsa frontend

--- comp_sec/algs/rsa.py
+++ comp_sec/algs/rsa.py
@@ -1,27 +1,37 @@
-p = 503
-q = 521
-n = p * q
-nt = 130520
-e = 3
-d = 43507
+a_p = 503
+a_q = 521
+a_n = a_p * a_q
+a_nt = 130520
+a_e = 3
+a_d = 43507
 
-def getConsts():
-    return p, q, n, nt, e, d
+b_p = 131
+b_q = 191
+b_n = b_p * b_q
+b_nt = 24700
+b_e = 7
+b_d = 17643
 
-def encrypt(message):
+def getConstsAlice():
+    return a_p, a_q, a_n, a_nt, a_e, a_d
+
+def getConstsBob():
+    return b_p, b_q, b_n, b_nt, b_e, b_d
+
+def encrypt(message, _e, _n):
     encrypted_message = ""
     
     for m in message:
         # c = m ** e % n
-        encrypted_message += str((ord(m) ** e) % n) + " "
+        encrypted_message += str((ord(m) ** _e) % _n) + " "
 
     return encrypted_message
 
-def decrypt(message):
+def decrypt(message, _d, _n):
     values = message.split()
     for i in range(len(values)):
         # m = c ** d % n
-        values[i] = chr((int(values[i]) ** d) % n)
+        values[i] = chr((int(values[i]) ** _d) % _n)
     decrypted_message = "".join(values)
 
     return decrypted_message
--- comp_sec/enc3.py
+++ comp_sec/enc3.py
@@ -7,7 +7,7 @@ from flask import request
 from flask import url_for
 from urllib.parse import parse_qs
 
-from comp_sec.algs.rsa import encrypt, decrypt, getConsts
+from comp_sec.algs.rsa import encrypt, decrypt, getConstsAlice, getConstsBob
 
 bp = Blueprint("enc3", __name__, url_prefix="/enc3")
 
@@ -24,23 +24,32 @@ def encrypter():
 
     parsed_form_data = parse_qs(form_data)
 
-    p, q, n, nt, e, d = getConsts()
-    p, q, n, nt, e, d = str(p), str(q), str(n), str(nt), str(e), str(d)
+    a_p, a_q, a_n, a_nt, a_e, a_d = getConstsAlice()
+    a_p, a_q, a_n, a_nt, a_e, a_d = str(a_p), str(a_q), str(a_n), str(a_nt), str(a_e), str(a_d)
+
+    b_p, b_q, b_n, b_nt, b_e, b_d = getConstsBob()
+    b_p, b_q, b_n, b_nt, b_e, b_d = str(b_p), str(b_q), str(b_n), str(b_nt), str(b_e), str(b_d)
 
     # alice submit
     if submitter == "0":
         if "alice_message" in parsed_form_data:
             alice_message = parsed_form_data["alice_message"][0]
-            encrypted_alice = encrypt(alice_message)
-            return {"alice": [" ", " ", " ", " ", " ", " ", " ", "SENDING: " + alice_message, "ENCRYPT: " + encrypted_alice, "SENT---------->", " "], "public": ["p = " + p, "q = " + q, "e = " + e, " ", "CALCULATIONS:", "n = p * q = " + n, "totient_n = " + nt, " ", " ", "PUBLIC: " + encrypted_alice, " "], "bob": [" ", " ", " ", " ", "CALCULATIONS:", "d = " + d + ", (from: 1 = e * d mod totient_n)", " ", " ", " ", "RECIEVED: " + encrypted_alice, "DECRYPT: " + decrypt(encrypted_alice)]}
+            encrypted_alice = encrypt(alice_message, int(b_e), int(b_n))
+            return {"alice": ["ALICE VARIABLES:", "a_p = " + a_p, "a_q = " + a_q, "a_n = a_p * a_q = " + a_n, "totient(a_n) = " + a_nt, "a_e = " + a_e, "a_d = " + a_d + "(from: 1 = a_e * a_d mod totient(a_n)", "SENDING: " + alice_message, "ENCRYPT (using c = m ^ b_e mod b_n): " + encrypted_alice, "SENT---------->", " "], 
+                    "public": [" ", " ", "PUBLIC INFO:", "a_n = " + a_n + ", b_n " + b_n, " ", "a_e = " + a_e + ", b_e = " + b_e, " ", " ", " ", "PUBLIC: " + encrypted_alice, " "], 
+                    "bob": ["BOB VARIABLES:", "b_p = " + b_p, "b_q = " + b_q, "b_n = b_p * b_q = " + b_n, "totient(b_n) = " + b_nt, "b_e = " + b_e, "b_d = " + b_d + "(from: 1 = b_e * b_d mod totient(b_n)", " ", " ", "RECIEVED: " + encrypted_alice, "DECRYPT (using m = c ^ b_d mod b_n): " + decrypt(encrypted_alice, int(b_d), int(b_n))]
+                    }
         else:
             return "Can not send from Alice with an empty Alice Message field."
     # bob submit
     elif submitter == "1":
         if "bob_message" in parsed_form_data:
             bob_message = parsed_form_data["bob_message"][0]
-            encrypted_bob = encrypt(bob_message)
-            return {"alice": [" ", " ", " ", " ", "CALCULATIONS:", "d = " + d + ", (from: 1 = e * d mod totient_n)", " ", " ", " ", "RECIEVED: " + encrypted_bob, "DECRYPT: " + decrypt(encrypted_bob)], "public": ["p = " + p, "q = " + q, "e = " + e, " ", "CALCULATIONS:", "n = p * q = " + n, "totient_n = " + nt, " ", " ", "PUBLIC: " + encrypted_bob, " "], "bob": [" ", " ", " ", " ", " ", " ", " ", "SENDING: " + bob_message, "ENCRYPT: " + encrypted_bob, "<-------------SENT", " "]}
+            encrypted_bob = encrypt(bob_message, int(a_e), int(a_n))
+            return {"alice": ["ALICE VARIABLES:", "a_p = " + a_p, "a_q = " + a_q, "a_n = a_p * a_q = " + a_n, "totient(a_n) = " + a_nt, "a_e = " + a_e, "a_d = " + a_d + "(from: 1 = a_e * a_d mod totient(a_n)", " ", " ", "RECIEVED: " + encrypted_bob, "DECRYPT (using m = c ^ a_d mod a_n): " + decrypt(encrypted_bob, int(a_d), int(a_n))], 
+                    "public": [" ", " ", "PUBLIC INFO:", "a_n = " + a_n + ", b_n " + b_n, " ", "a_e = " + a_e + ", b_e = " + b_e, " ", " ", " ", "PUBLIC: " + encrypted_bob, " "], 
+                    "bob": ["BOB VARIABLES:", "b_p = " + b_p, "b_q = " + b_q, "b_n = b_p * b_q = " + b_n, "totient(b_n) = " + b_nt, "b_e = " + b_e, "b_d = " + b_d + "(from: 1 = b_e * b_d mod totient(b_n)", "SENDING: " + bob_message, "ENCRYPT (using c = m ^ a_e mod a_n): " + encrypted_bob, "<-------------SENT", " "]
+                    }
         else:
             return "Can not send from Bob with an empty Bob Message field."
     return "ERROR"
